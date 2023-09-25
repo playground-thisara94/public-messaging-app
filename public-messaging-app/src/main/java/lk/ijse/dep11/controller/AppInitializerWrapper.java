0fb61f3cf9600967136243f261872685d668212a
Thisara Rathnapala <thisaratr94@gmail.com>
2023-09-25 18:06:25 +0530

    Begin Client App

---
+++ public-messaging-app/src/main/java/lk/ijse/dep11/controller/AppInitializerWrapper.java
@@ -0,0 +1,9 @@
+package lk.ijse.dep11.controller;
+
+import lk.ijse.dep11.AppInitializer;
+
+public class AppInitializerWrapper {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
